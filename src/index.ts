34b49885f0b1f4c949c45ffbd8c203bcaddd5388
Dimash <dinmukhamed.amirov@gmail.com>
2024-11-01 10:51:56 +0500

    feat: change port

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import userRouter from "./routes/userRouter";
 import postRouter from "./routes/postRouter";
 import authRouter from "./routes/authRouter";
 import fileUpload from "express-fileupload";
+import path from "path";
 
 const app = express();
 app.use(cors());
@@ -13,14 +14,17 @@ app.use(userRouter);
 app.use(postRouter);
 app.use(authRouter);
 app.use(fileUpload({}));
+app.use(express.static("uploads"));
+
+console.log(path.join(__dirname, "uploads"));
 
 // Инициализация подключения к базе данных
 AppDataSource.initialize()
   .then(() => {
     console.log("Data Source has been initialized!");
 
-    app.listen(5000, () => {
-      console.log("Server is running on port 5000");
+    app.listen(8080, () => {
+      console.log("Server is running on port 8080");
     });
   })
   .catch(error =>
